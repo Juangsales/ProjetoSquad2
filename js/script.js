07e58c300f40f889cf4eb23b2b46ea9c7f79c342
Leonardo Macena <113394226+leomacena@users.noreply.github.com>
2022-09-27 12:46:46 -0300

    validação de contato

--- js/script.js
+++ js/script.js
@@ -10,4 +10,19 @@ window.onscroll = function () { myFunction() };
         } else {
           bar.classList.remove("sticky");
         }
-      }          
+      }      
+
+function Enviar() {
+
+    var nome = document.getElementById("nome");
+    var fone = document.getElementById("tel");
+    var mail = document.getElementById("email");
+    var text = document.getElementById("text");
+
+    if (text.value == "" && (mail.value =="" && (fone.value == "" && nome.value == ""))) {
+            
+    } else{
+        alert('Obrigado Sr(a) ' + nome.value + ', a sua mensagem foi enviada com sucesso');
+    }
+
+}
